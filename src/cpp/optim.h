cc3046f933fcd9d1a260aaac1d30f83e524cadbe
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-09-12 16:19:01 +0900

    Add template parameters in svdSolve function

--- src/cpp/optim.h
+++ src/cpp/optim.h
@@ -3,14 +3,15 @@
 #include <Eigen/SVD>
 
 // Solve Ax = 0
-Eigen::Vector<float, 16> svdSolve(const Eigen::Matrix<float, Eigen::Dynamic, 16> &A)
+template <typename T = float, int N = 16>
+Eigen::Vector<T, N> svdSolve(const Eigen::Matrix<T, Eigen::Dynamic, N> &A)
 {
     // https://eigen.tuxfamily.org/dox/group__SVD__Module.html
     Eigen::JacobiSVD<Eigen::Matrix<float, Eigen::Dynamic, 16>> svd(A, Eigen::ComputeThinV);
     // Eigen::BDCSVD<Eigen::MatrixXf> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
-    auto V = svd.matrixV();                           // Get V matrix
-    Eigen::Vector<float, 16> x = V.col(V.cols() - 1); // Get last column
-    float div_x0 = 1.0 / x(0);                        // Precompute 1/x0
-    x *= div_x0;                                      // Normalize the vector
+    auto V = svd.matrixV();                      // Get V matrix
+    Eigen::Vector<T, N> x = V.col(V.cols() - 1); // Get last column
+    float div_x0 = 1.0 / x(0);                   // Precompute 1/x0
+    x *= div_x0;                                 // Normalize the vector
     return x;
 }
