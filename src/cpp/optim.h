bf1cc8138021e33982908ad73671c363ffa76737
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-09-12 15:58:07 +0900

    Remove unnecessary cpp files

--- src/cpp/optim.h
+++ src/cpp/optim.h
@@ -1,13 +1,20 @@
 #pragma once
-#include <iostream>
-#include <chrono>
-#include <utility>
 #include <random>
 
-#include <omp.h>
 #include <Eigen/Core>
 #include <Eigen/SVD>
 
 #include "equations.h"
 
-Eigen::Vector<float, 16> svdSolve(const Eigen::Matrix<float, Eigen::Dynamic, 16> &A);
+// Solve Ax = 0
+Eigen::Vector<float, 16> svdSolve(const Eigen::Matrix<float, Eigen::Dynamic, 16> &A)
+{
+    // https://eigen.tuxfamily.org/dox/group__SVD__Module.html
+    Eigen::JacobiSVD<Eigen::Matrix<float, Eigen::Dynamic, 16>> svd(A, Eigen::ComputeThinV);
+    // Eigen::BDCSVD<Eigen::MatrixXf> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    auto V = svd.matrixV();                           // Get V matrix
+    Eigen::Vector<float, 16> x = V.col(V.cols() - 1); // Get last column
+    float div_x0 = 1.0 / x(0);                        // Precompute 1/x0
+    x *= div_x0;                                      // Normalize the vector
+    return x;
+}
