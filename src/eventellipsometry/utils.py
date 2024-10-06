aa18fd36efda76ed9dc4b534d6240d305d3caba6
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-10-06 16:32:11 +0900

    Refactor get_num_after_keyword function to accept multiple keywords

--- src/eventellipsometry/utils.py
+++ src/eventellipsometry/utils.py
@@ -1,9 +1,9 @@
 import re
 from pathlib import Path
-from typing import Union
+from typing import Union, List
 
 
-def get_num_after_keyword(string: Union[str, Path], keyword: str) -> Union[int, float]:
+def _get_num_after_keyword(string: Union[str, Path], keyword: str) -> Union[int, float]:
     """Extract a number in the string located after the specific keyword.
 
     Parameters
@@ -42,3 +42,36 @@ def get_num_after_keyword(string: Union[str, Path], keyword: str) -> Union[int, 
         number = int(number)
 
     return number
+
+
+def get_num_after_keyword(string: Union[str, Path], keywords: Union[str, List[str]]) -> Union[Union[int, float], List[Union[int, float]]]:
+    """Extract a number in the string located after the specific keyword.
+
+    Parameters
+    ----------
+    string : Union[str, Path]
+        The string to be searched.
+    keywords : List[str]
+        The keywords to be searched.
+
+    Returns
+    -------
+    number : Union[Union[int, float], List[Union[int, float]]]
+        The extracted number.
+
+    Examples
+    --------
+    >>> string = "00162 tl015 pl060 tv000 pv000.jpg"
+    >>> tl, pl = get_num_after_keyword(string, ["tl", "pl"])
+    >>> tl
+        15
+    >>> pl
+        60
+    >>> tv = get_num_after_keyword(string, "tv")
+    >>> tv
+        0
+    """
+    if isinstance(keywords, str):
+        return _get_num_after_keyword(string, keywords)
+    else:
+        return [_get_num_after_keyword(string, keyword) for keyword in keywords]
