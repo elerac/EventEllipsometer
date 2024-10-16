9b8ac2ed55adcfeefa26fa0edf29cc7797b3afa9
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-10-16 14:33:16 +0900

    Refactor .gitignore to include recordings directory and filenames.py

---
+++ recordings/filenames.py
@@ -0,0 +1,66 @@
+filename_raw = "recordings/recording_2024-07-30_14-29-23.raw"  # 0, 30Hz
+# filename_raw = "recordings/recording_2024-07-29_21-48-46.raw"  # -10, 30Hz
+# filename_raw = "recordings/recording_2024-07-29_21-51-08.raw"  # -20, 30Hz, i=5 good
+filename_raw = "recordings/recording_2024-07-30_13-32-46.raw"  # -30, 30Hz
+filename_raw = "recordings/recording_2024-07-30_13-39-26.raw"  # -30, 48Hz
+# filename_raw = "recordings/recording_2024-07-30_13-43-17.raw"  # -30, 20Hz
+# filename_raw = "recordings/recording_2024-07-30_15-45-54.raw"  # -20, 30Hz
+filename_raw = "recordings/recording_2024-07-30_16-06-55.raw"  # -20, 40Hz
+filename_raw = "recordings/recording_2024-07-30_16-42-37.raw"  # -20, 40Hz, weak light
+filename_raw = "recordings/recording_2024-07-30_19-08-37.raw"  # -20, 30Hz, linear polarizer +90
+# filename_raw = "recordings/recording_2024-07-30_18-54-31.raw"  # -20, 40Hz, linear polarizer +0
+# filename_raw = "recordings/recording_2024-07-31_13-55-06.raw"  # -20, 30Hz, air, 1
+# filename_raw = "recordings/recording_2024-07-31_13-56- .raw"  # -20, 30Hz, air, 2, bounary
+# filename_raw = "recordings/recording_2024-07-31_13-57-14.raw"  # -20, 30Hz, air, 3
+# filename_raw = "recordings/recording_2024-07-31_13-57-45.raw"  # -20, 30Hz, air, 4
+# filename_raw = "recordings/recording_2024-07-31_16-12-40.raw"  # -20, 30Hz, air, 5
+# filename_raw = "recordings/recording_2024-07-31_16-13-27.raw"  # -20, 30Hz, air, 6
+# filename_raw = "recordings/recording_2024-07-31_16-15-32.raw"  # -20, 30Hz, air, 7
+# filename_raw = "recordings/recording_2024-07-31_16-15-59.raw"  # -20, 30Hz, air, 8
+# filename_raw = "recordings/recording_2024-07-31_16-17-01.raw"  # -20, 30Hz, air, 9
+# filename_raw = "recordings/recording_2024-07-31_16-17-49.raw"  # -20, 30Hz, air, 10
+
+filename_raw = "recordings/recording_2024-08-05_20-00-19.raw"  # -20, 30Hz, air1, 100x100
+# filename_raw = "recordings/recording_2024-08-05_20-01-21.raw"  # -20, 30Hz, air2, 100x100
+# filename_raw = "recordings/recording_2024-08-05_20-01-33.raw"  # -20, 30Hz, air3, 100x100
+# filename_raw = "recordings/recording_2024-08-05_20-02-26.raw"  # -20, 30Hz, air1, 50x50
+# filename_raw = "recordings/recording_2024-08-05_20-03-08.raw"  # -20, 30Hz, air2, 50x50
+# filename_raw = "recordings/recording_2024-08-05_20-03-40.raw"  # -20, 30Hz, air3, 50x50
+# filename_raw = "recordings/recording_2024-08-05_20-04-36.raw"  # -20, 30Hz, air1, 25x25
+# filename_raw = "recordings/recording_2024-08-05_20-04-59.raw"  # -20, 30Hz, air2, 25x25
+# filename_raw = "recordings/recording_2024-08-05_20-05-49.raw"  # -20, 30Hz, air3, 25x25
+
+# filename_raw = "recordings/recording_2024-08-05_20-09-14.raw"  # -20, 30Hz, polarizer0, 100x100
+# filename_raw = "recordings/recording_2024-08-05_20-08-40.raw"  # -20, 30Hz, polarizer0, 50x50
+# filename_raw = "recordings/recording_2024-08-05_20-08-07.raw"  # -20, 30Hz, polarizer0, 25x25
+
+# filename_raw = "recordings/recording_2024-08-05_20-10-39.raw"  # -20, 30Hz, polarizer90, 100x100
+# filename_raw = "recordings/recording_2024-08-05_20-11-01.raw"  # -20, 30Hz, polarizer90, 50x50
+# filename_raw = "recordings/recording_2024-08-05_20-11-46.raw"  # -20, 30Hz, polarizer90, 25x25
+
+# New calibration
+# -------------------------
+# filename_raw = "recordings/recording_2024-08-21_20-09-27.raw"  # -20, 30Hz, air, 100x100
+# filename_raw = "recordings/recording_2024-08-21_21-25-14.raw"  # -20, 30Hz, polarizer90, 512x512
+# filename_raw = "recordings/recording_2024-08-21_21-40-23.raw"  # -20, 30Hz, polarizer90, 512x512, motor direction of light side has flipped
+# filename_raw = "recordings/recording_2024-08-22_11-08-01.raw"
+# filename_raw = "recordings/recording_2024-08-22_11-25-44.raw"  # -30, 30H, lens
+# filename_raw = "recordings/recording_2024-08-22_12-54-28.raw"  # -30, 30H, metal
+# filename_raw = "recordings/recording_2024-08-22_13-40-15.raw"  # -30, 30H, metal, dark
+
+# New calibration
+# -------------------------
+# filename_raw = "recordings/recording_2024-08-25_15-33-02.raw"  # -30, 30Hz, air2, 100x100
+# filename_raw = "recordings/recording_2024-08-25_15-47-20.raw"  # -30, 30Hz, air2 (+green filter), 100x100,
+# filename_raw = "recordings/recording_2024-08-25_17-36-09.raw"  # -30, 30Hz, air2 (+green filter), 10x10, 2nd
+# filename_raw = "recordings/recording_2024-08-25_20-13-57.raw"  # -20, 30Hz, air , 100x100
+# filename_raw = "recordings/recording_2024-08-25_20-22-03.raw"  # -20, 30Hz, air (umpbrella), 100x100
+# filename_raw = "recordings/recording_2024-08-25_20-26-24.raw"  # -20, 30Hz, air (umpbrella), 100x100, 2nd
+# filename_raw = "recordings/recording_2024-08-25_20-28-44.raw"  # -20, 30Hz, air (umpbrella), 100x100, 3rd
+# filename_raw = "recordings/recording_2024-08-25_20-30-28.raw"  # -20, 30Hz, air (umpbrella), 100x100, 4th
+# filename_raw = "recordings/recording_2024-08-25_20-49-55.raw"  # -20, 30Hz, air, 540nm, 100x100, 5th
+# filename_raw = "recordings/recording_2024-08-25_21-05-55.raw"  # -30, 30Hz, air, 540nm, 100x100, 6th
+# filename_raw = "recordings/recording_2024-08-25_21-07-26.raw"  # -30, 30Hz, air, 540nm, 100x100, 7th
+# filename_raw = "recordings/recording_2024-08-25_21-31-16.raw"  # -20, 30Hz, air, 540nm, 512x512, 8th
+filename_raw = "recordings/recording_2024-08-27_21-54-17.raw"  # -20, 30Hz, metal, 100x100, 4th (good)
+# filename_raw = "recordings/recording_2024-08-27_22-16-54.raw"  # -30, 30Hz, NVIDIA GPU, 512x512, good
