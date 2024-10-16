93d1883e9d1b931c4e392f68cc7fbcda4d89a490
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-10-16 15:16:49 +0900

    Refactor optimize_video.py to use calibration utility code

--- optimize_video.py
+++ optimize_video.py
@@ -6,6 +6,7 @@ import cv2
 import polanalyser as pa
 import eventellipsometry as ee
 from recordings.filenames import filename_raw
+import calib
 
 
 def main():
@@ -60,19 +61,16 @@ def main():
     phi1 = np.deg2rad(6.5)
     phi2 = np.deg2rad(62.5)
 
-    # filename_calib_phi1 = "calib/qwp_offset/phi1.txt"
-    # filename_calib_phi2 = "calib/qwp_offset/phi2.txt"
-    # phi1 = float(np.loadtxt(filename_calib_phi1, comments="%"))
-    # phi2 = float(np.loadtxt(filename_calib_phi2, comments="%"))
+    # phi1 = calib.phi1
+    # phi2 = calib.phi2
+
     print(f"    phi1: {np.rad2deg(phi1):.2f} ({phi1:.2f})")
     print(f"    phi2: {np.rad2deg(phi2):.2f} ({phi2:.2f})")
     print(f"  Thresholds")
-    filename_C_on = f"calib/thresh/on/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
-    filename_C_off = f"calib/thresh/off/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
-    print(f"    Con : {filename_C_on}")
-    print(f"    Coff: {filename_C_off}")
-    img_C_on = np.load(filename_C_on).astype(np.float32)
-    img_C_off = np.load(filename_C_off).astype(np.float32)
+    print(f"    Con : {bias_diff=}, {bias_diff_on=}")
+    print(f"    Coff: {bias_diff=}, {bias_diff_off=}")
+    img_C_on = calib.thresh_on(bias_diff, bias_diff_on)
+    img_C_off = calib.thresh_off(bias_diff, bias_diff_off)
 
     print("Construct dataframes")
     ellipsometry_eventmaps = ee.construct_dataframes(events["x"], events["y"], events["t"], events["p"], width, height, trig_t, trig_p, img_C_on, img_C_off)
@@ -93,59 +91,59 @@ def main():
     )
     video_mm = np.array(video_mm)
     print(f"Total reconstruction time: {time.time() - time_start:.2f} s")
+    num_frames, height, width, _, _ = video_mm.shape
 
     print("Save Mueller matrix video")
     filename_npz = Path("mmvideos") / f"{Path(args.filename_raw).stem}_iter_svd{args.max_iter_svd}_tol{args.tol}_iter_propagate{args.max_iter_propagate}.npz"
     filename_npz.parent.mkdir(parents=True, exist_ok=True)
     np.savez_compressed(filename_npz.with_suffix(".npz"), video_mm=video_mm)
-    print(f"  {filename_npz}")
-    exit()
+    print(f"  npz: {filename_npz}")
+    # exit()
 
     # Visualize
-    num_frames, height, width, _, _ = video_mm.shape
-    video_mae = np.full((num_frames, height, width), np.nan)  # Video of mean absolute error
-    video_color = np.zeros((num_frames, height, width, 3))  # Video of color
-    for f in range(num_frames):
-        for y in range(height):
-            for x in range(width):
-                theta, dlogI, weights, phi_offset = ellipsometry_eventmaps[f].get(x, y)
-                if len(theta) == 0:
-                    continue
+    # video_mae = np.full((num_frames, height, width), np.nan)  # Video of mean absolute error
+    # video_color = np.zeros((num_frames, height, width, 3))  # Video of color
+    # for f in range(num_frames):
+    #     for y in range(height):
+    #         for x in range(width):
+    #             theta, dlogI, weights, phi_offset = ellipsometry_eventmaps[f].get(x, y)
+    #             if len(theta) == 0:
+    #                 continue
 
-                n_coffs, d_coffs = ee.calculate_ndcoffs(theta, phi1, phi2 - 5 * phi_offset)
-                M = video_mm[f, y, x]
-                dlogI_pred = (n_coffs @ M.flatten()) / (d_coffs @ M.flatten())
-                r = np.abs(dlogI - dlogI_pred)
-                mae = np.mean(r * weights)
-                video_mae[f, y, x] = mae
+    #             n_coffs, d_coffs = ee.calculate_ndcoffs(theta, phi1, phi2 - 5 * phi_offset)
+    #             M = video_mm[f, y, x]
+    #             dlogI_pred = (n_coffs @ M.flatten()) / (d_coffs @ M.flatten())
+    #             r = np.abs(dlogI - dlogI_pred)
+    #             mae = np.mean(r * weights)
+    #             video_mae[f, y, x] = mae
 
-                p = np.sign(dlogI)
-                color_pos = np.array([0.0, 0.0, 1.0])
-                color_neg = np.array([1.0, 0.0, 0.0])
-                # inv_r = 1 / r
-                # inv_r_normalized = inv_r
-                # r_pecentile = np.percentile(r, 99)
-                # invalid = r > r_pecentile
-                w_pos = (p == 1) / p.size * r**2 / 100
-                w_neg = (p == -1) / p.size * r**2 / 100
-                color = color_pos * w_pos[:, None] + color_neg * w_neg[:, None]
-                video_color[f, y, x] = np.sum(color, axis=0)
+    #             p = np.sign(dlogI)
+    #             color_pos = np.array([0.0, 0.0, 1.0])
+    #             color_neg = np.array([1.0, 0.0, 0.0])
+    #             # inv_r = 1 / r
+    #             # inv_r_normalized = inv_r
+    #             # r_pecentile = np.percentile(r, 99)
+    #             # invalid = r > r_pecentile
+    #             w_pos = (p == 1) / p.size * r**2 / 100
+    #             w_neg = (p == -1) / p.size * r**2 / 100
+    #             color = color_pos * w_pos[:, None] + color_neg * w_neg[:, None]
+    #             video_color[f, y, x] = np.sum(color, axis=0)
 
-    print(f"Mean absolute error: {np.nanmean(video_mae):.3f}")
-    vmin = np.percentile(video_mae[~np.isnan(video_mae)], 1)
-    vmax = np.percentile(video_mae[~np.isnan(video_mae)], 99)
-    video_mae_vis = pa.applyColorMap(video_mae, "inferno", vmin=vmin, vmax=vmax)
-    cv2.imwrite("mae.png", video_mae_vis[0])
+    # print(f"Mean absolute error: {np.nanmean(video_mae):.3f}")
+    # vmin = np.percentile(video_mae[~np.isnan(video_mae)], 1)
+    # vmax = np.percentile(video_mae[~np.isnan(video_mae)], 99)
+    # video_mae_vis = pa.applyColorMap(video_mae, "inferno", vmin=vmin, vmax=vmax)
+    # cv2.imwrite("mae.png", video_mae_vis[0])
 
     # exit()
 
     # Extract non-zero elements and print
-    print(video_color[video_color != 0])
+    # print(video_color[video_color != 0])
 
-    cv2.imwrite("color.png", np.clip(video_color[0] * 255, 0, 255).astype(np.uint8))
+    # cv2.imwrite("color.png", np.clip(video_color[0] * 255, 0, 255).astype(np.uint8))
 
     print("Visualize")
-    img_mueller_vis_list = [ee.mueller_image(video_mm[i]) for i in range(num_frames)]
+    img_mueller_vis_list = [ee.mueller_image(video_mm[i]) for i in range(len(video_mm))]
 
     print("Save")
     for i, img_mueller_vis in enumerate(img_mueller_vis_list):
