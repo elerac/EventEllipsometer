0c90cdf3f682846a063a2c6495e73a2ef08b5aee
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-10-16 21:49:42 +0900

    Refactor optimize_video.py to integrate synthetic data input as same pipeline

--- optimize_video.py
+++ optimize_video.py
@@ -24,71 +24,76 @@ def main():
     print("Read event data")
     print(f"  {args.filename_raw}")
     events = ee.read_event(args.filename_raw)
-
-    bias_diff = int(events["bias_diff"])
-    bias_diff_on = int(events["bias_diff_on"])
-    bias_diff_off = int(events["bias_diff_off"])
-    print(f"  Bias diff: {bias_diff}")
-    print(f"  Bias diff on: {bias_diff_on}")
-    print(f"  Bias diff off: {bias_diff_off}")
     width = events["width"]
     height = events["height"]
 
-    # Triggers (jus for checking)
-    trig_t = events["trig_t"]
-    trig_p = events["trig_p"]
-    trig_t_x1 = trig_t[trig_p == 0]  # OFF
-    trig_t_x5 = trig_t[trig_p == 1]  # ON
-    trig_t_x1, trig_t_x5 = ee.clean_triggers(trig_t_x1, trig_t_x5)
-    period = np.diff(trig_t_x1)
-    period = np.append(period, period[-1])  # Add last element
-    trig_t_diff = trig_t_x5 - trig_t_x1  # [0, period / 5]
-    phi_offsets = trig_t_diff / period * np.pi
-    print(f"  Triggers: {len(trig_t_x1)=}, {len(trig_t_x5)=}")
-    print(f"  Avg. frequency: {1 / np.mean(period) * 1000000:.2f} fps")
-    # for i, (trig_t_x1_i, trig_t_x5_i, trig_t_diff_i, period_i, phi_offset_i) in enumerate(zip(trig_t_x1, trig_t_x5, trig_t_diff, period, phi_offsets)):
-    #     print(f"Triggers {i}: {trig_t_x1_i:d}, {trig_t_x5_i:d}, {trig_t_diff_i:d}, {period_i:d}, {np.rad2deg(phi_offset_i):.2f}")
+    mode = events.get("mode", None)
+    print(f"  Mode: {mode}")
 
-    # Calib
-    print("Calibration parameters")
-    print(f"  QWP offset")
-    phi1 = np.deg2rad(56)
-    phi2 = np.deg2rad(146)
+    if mode == "synthetic":
+        C = float(events["thresh"])
+        img_C_on = C
+        img_C_off = -C
+        phi1 = 0
+        phi2 = 0
+        print("Thresholds")
+        print(f"  Con : {C:.3f}")
+        print(f"  Coff: {-C:.3f}")
+    else:
+        bias_diff = int(events["bias_diff"])
+        bias_diff_on = int(events["bias_diff_on"])
+        bias_diff_off = int(events["bias_diff_off"])
+        print(f"  Bias diff: {bias_diff}")
+        print(f"  Bias diff on: {bias_diff_on}")
+        print(f"  Bias diff off: {bias_diff_off}")
 
-    # phi1 = np.deg2rad(6.5)
-    # phi2 = np.deg2rad(64)
+        # Triggers (jus for checking)
+        trig_t = events["trig_t"]
+        trig_p = events["trig_p"]
+        trig_t_x1 = trig_t[trig_p == 0]  # OFF
+        trig_t_x5 = trig_t[trig_p == 1]  # ON
+        trig_t_x1, trig_t_x5 = ee.clean_triggers(trig_t_x1, trig_t_x5)
+        period = np.diff(trig_t_x1)
+        period = np.append(period, period[-1])  # Add last element
+        trig_t_diff = trig_t_x5 - trig_t_x1  # [0, period / 5]
+        phi_offsets = trig_t_diff / period * np.pi
+        print(f"  Triggers: {len(trig_t_x1)=}, {len(trig_t_x5)=}")
+        print(f"  Avg. frequency: {1 / np.mean(period) * 1000000:.2f} fps")
+        # for i, (trig_t_x1_i, trig_t_x5_i, trig_t_diff_i, period_i, phi_offset_i) in enumerate(zip(trig_t_x1, trig_t_x5, trig_t_diff, period, phi_offsets)):
+        #     print(f"Triggers {i}: {trig_t_x1_i:d}, {trig_t_x5_i:d}, {trig_t_diff_i:d}, {period_i:d}, {np.rad2deg(phi_offset_i):.2f}")
 
-    phi1 = np.deg2rad(6.5)
-    phi2 = np.deg2rad(62.5)
+        # Calib
+        print("Calibration parameters")
+        print(f"  QWP offset")
+        phi1 = np.deg2rad(56)
+        phi2 = np.deg2rad(146)
 
-    # phi1 = calib.phi1
-    # phi2 = calib.phi2
+        # phi1 = np.deg2rad(6.5)
+        # phi2 = np.deg2rad(64)
 
-    print(f"    phi1: {np.rad2deg(phi1):.2f} ({phi1:.2f})")
-    print(f"    phi2: {np.rad2deg(phi2):.2f} ({phi2:.2f})")
-    print(f"  Thresholds")
-    print(f"    Con : {bias_diff=}, {bias_diff_on=}")
-    print(f"    Coff: {bias_diff=}, {bias_diff_off=}")
-    img_C_on = calib.thresh_on(bias_diff, bias_diff_on)
-    img_C_off = calib.thresh_off(bias_diff, bias_diff_off)
+        phi1 = np.deg2rad(6.5)
+        phi2 = np.deg2rad(62.5)
+
+        # phi1 = calib.phi1
+        # phi2 = calib.phi2
+
+        print(f"    phi1: {np.rad2deg(phi1):.2f} ({phi1:.2f})")
+        print(f"    phi2: {np.rad2deg(phi2):.2f} ({phi2:.2f})")
+        print(f"  Thresholds")
+        print(f"    Con : {bias_diff=}, {bias_diff_on=}")
+        print(f"    Coff: {bias_diff=}, {bias_diff_off=}")
+        img_C_on = calib.thresh_on(bias_diff, bias_diff_on)
+        img_C_off = calib.thresh_off(bias_diff, bias_diff_off)
 
     print("Construct dataframes")
-    ellipsometry_eventmaps = ee.construct_dataframes(events["x"], events["y"], events["t"], events["p"], width, height, trig_t, trig_p, img_C_on, img_C_off)
+    ellipsometry_eventmaps = ee.construct_dataframes(events["x"], events["y"], events["t"], events["p"], width, height, events["trig_t"], events["trig_p"], img_C_on, img_C_off)
     if args.max_frames is not None:
         ellipsometry_eventmaps = ellipsometry_eventmaps[: args.max_frames]
     print(f"  Number of frames: {len(ellipsometry_eventmaps)}")
 
     print("Start reconstruction")
     time_start = time.time()
-    video_mm = ee.fit_mueller(
-        ellipsometry_eventmaps,
-        phi1,
-        phi2,
-        max_iter_svd=args.max_iter_svd,
-        tol=args.tol,
-        max_iter_propagate=args.max_iter_propagate,
-        verbose=True,
-    )
+    video_mm = ee.fit_mueller(ellipsometry_eventmaps, phi1, phi2, max_iter_svd=args.max_iter_svd, tol=args.tol, max_iter_propagate=args.max_iter_propagate, verbose=True)
     video_mm = np.array(video_mm)
     print(f"Total reconstruction time: {time.time() - time_start:.2f} s")
     num_frames, height, width, _, _ = video_mm.shape
@@ -143,7 +148,7 @@ def main():
     # cv2.imwrite("color.png", np.clip(video_color[0] * 255, 0, 255).astype(np.uint8))
 
     print("Visualize")
-    img_mueller_vis_list = [ee.mueller_image(video_mm[i]) for i in range(len(video_mm))]
+    img_mueller_vis_list = [ee.mueller_image(pa.gammaCorrection(video_mm[i], 1 / 1.8)) for i in range(len(video_mm))]
 
     print("Save")
     for i, img_mueller_vis in enumerate(img_mueller_vis_list):
