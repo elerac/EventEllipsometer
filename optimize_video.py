7ba5c94e1ca855f8c4a10f8c3540312d0ab4bdeb
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-10-16 14:28:14 +0900

    Refactor optimize_video.py to improve code structure and add command line arguments for flexibility

--- optimize_video.py
+++ optimize_video.py
@@ -1,5 +1,6 @@
 import argparse
 import time
+from pathlib import Path
 import numpy as np
 import cv2
 import polanalyser as pa
@@ -11,9 +12,17 @@ def main():
     np.set_printoptions(precision=3, suppress=True)
     np.random.seed(0)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename_raw", type=str, default=filename_raw)
+    parser.add_argument("--max_frames", type=int, default=None)
+    parser.add_argument("--max_iter_svd", type=int, default=10)
+    parser.add_argument("--tol", type=float, default=0.01)
+    parser.add_argument("--max_iter_propagate", type=int, default=10)
+    args = parser.parse_args()
+
     print("Read event data")
-    print(filename_raw)
-    events = ee.read_event(filename_raw)
+    print(f"  {args.filename_raw}")
+    events = ee.read_event(args.filename_raw)
 
     bias_diff = int(events["bias_diff"])
     bias_diff_on = int(events["bias_diff_on"])
@@ -23,10 +32,6 @@ def main():
     print(f"  Bias diff off: {bias_diff_off}")
     width = events["width"]
     height = events["height"]
-    filename_C_on = f"calib/thresh/on/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
-    filename_C_off = f"calib/thresh/off/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
-    img_C_on = np.load(filename_C_on).astype(np.float32)
-    img_C_off = np.load(filename_C_off).astype(np.float32)
 
     # Triggers (jus for checking)
     trig_t = events["trig_t"]
@@ -38,8 +43,8 @@ def main():
     period = np.append(period, period[-1])  # Add last element
     trig_t_diff = trig_t_x5 - trig_t_x1  # [0, period / 5]
     phi_offsets = trig_t_diff / period * np.pi
-    print(f"Triggers:", len(trig_t_x1), len(trig_t_x5))
-    print(f"Avg. frequency: {1 / np.mean(period) * 1000000:.2f} fps")
+    print(f"  Triggers: {len(trig_t_x1)=}, {len(trig_t_x5)=}")
+    print(f"  Avg. frequency: {1 / np.mean(period) * 1000000:.2f} fps")
     # for i, (trig_t_x1_i, trig_t_x5_i, trig_t_diff_i, period_i, phi_offset_i) in enumerate(zip(trig_t_x1, trig_t_x5, trig_t_diff, period, phi_offsets)):
     #     print(f"Triggers {i}: {trig_t_x1_i:d}, {trig_t_x5_i:d}, {trig_t_diff_i:d}, {period_i:d}, {np.rad2deg(phi_offset_i):.2f}")
 
@@ -58,25 +63,47 @@ def main():
     # phi1 = float(np.loadtxt(filename_calib_phi1, comments="%"))
     # phi2 = float(np.loadtxt(filename_calib_phi2, comments="%"))
 
-    print("Calibration")
-    print(f"phi1: {np.rad2deg(phi1):.2f} ({phi1:.2f})")
-    print(f"phi2: {np.rad2deg(phi2):.2f} ({phi2:.2f})")
+    print("Calibration parameters")
+    print(f"  QWP offset")
+    print(f"    phi1: {np.rad2deg(phi1):.2f} ({phi1:.2f})")
+    print(f"    phi2: {np.rad2deg(phi2):.2f} ({phi2:.2f})")
+    filename_C_on = f"calib/thresh/on/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
+    filename_C_off = f"calib/thresh/off/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
+    print(f"  Thresholds")
+    print(f"    Con : {filename_C_on}")
+    print(f"    Coff: {filename_C_off}")
+    img_C_on = np.load(filename_C_on).astype(np.float32)
+    img_C_off = np.load(filename_C_off).astype(np.float32)
 
-    time_start = time.time()
+    print("Construct dataframes")
     ellipsometry_eventmaps = ee.construct_dataframes(events["x"], events["y"], events["t"], events["p"], width, height, trig_t, trig_p, img_C_on, img_C_off)
-    ellipsometry_eventmaps = ellipsometry_eventmaps[:1]
-    time_end = time.time()
-    print(f"Time (dataframes): {time_end - time_start:.2f} s")
+    if args.max_frames is not None:
+        ellipsometry_eventmaps = ellipsometry_eventmaps[: args.max_frames]
+    print(f"  Number of frames: {len(ellipsometry_eventmaps)}")
 
-    print("Fit")
+    print("Start reconstruction")
     time_start = time.time()
-    video_mm = ee.fit_mueller(ellipsometry_eventmaps, phi1, phi2, max_iter_svd=10, tol=0.001, max_iter_propagate=15, verbose=True)
-    video_mm = np.array(video_mm)  # Ensure numpy array (nanobind's default typing is ArrayLike)
-    print(f"Time: {time.time() - time_start:.2f} s")
+    video_mm = ee.fit_mueller(
+        ellipsometry_eventmaps,
+        phi1,
+        phi2,
+        max_iter_svd=args.max_iter_svd,
+        tol=args.tol,
+        max_iter_propagate=args.max_iter_propagate,
+        verbose=True,
+    )
+    video_mm = np.array(video_mm)
+    print(f"Total reconstruction time: {time.time() - time_start:.2f} s")
 
-    print(video_mm.shape, video_mm.dtype)
+    print("Save Mueller matrix video")
+    filename_npz = Path("mmvideo") / f"{Path(args.filename_raw).stem}_iter_svd{args.max_iter_svd}_tol{args.tol}_iter_propagate{args.max_iter_propagate}.npz"
+    filename_npz.parent.mkdir(parents=True, exist_ok=True)
+    np.savez_compressed(filename_npz.with_suffix(".npz"), video_mm=video_mm)
+    print(f"  {filename_npz}")
+    exit()
+
+    # Visualize
     num_frames, height, width, _, _ = video_mm.shape
-
     video_mae = np.full((num_frames, height, width), np.nan)  # Video of mean absolute error
     video_color = np.zeros((num_frames, height, width, 3))  # Video of color
     for f in range(num_frames):
