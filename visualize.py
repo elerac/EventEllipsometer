70851f469f9f2c92a99b84da215c79552e353d9e
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-10-26 16:30:40 +0900

    Refactor visualize.py to add video writing functionality

--- visualize.py
+++ visualize.py
@@ -1,8 +1,28 @@
 from pathlib import Path
 import numpy as np
+import re
 import cv2
 import polanalyser as pa
 import eventellipsometry as ee
+
+
+# sort by filename (numerically)
+def sort_key(s):
+    return [int(t) if t.isdigit() else t for t in re.split(r"(\d+)", s.name)]
+
+
+def vidwrite(filename, images, framerate=30):
+    filename = Path(filename)
+    filename.parent.mkdir(exist_ok=True, parents=True)
+
+    images = np.array(images)
+    num, height, width, ch = images.shape
+    size = (width, height)
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    vid = cv2.VideoWriter(str(filename), fourcc, framerate, size)
+    for image in images:
+        vid.write(image)
+    vid.release()
 
 
 def visualize_mueller_video(filename_npy):
@@ -18,14 +38,21 @@ def visualize_mueller_video(filename_npy):
     x0, x1 = np.where(~img_is_nan)[1][[0, -1]]
     video_mm = video_mm[:, y0:y1, x0:x1]
 
+    # Mueller matrix images (with median text)
     img_mueller_vis_list = [ee.mueller_image(pa.gammaCorrection(video_mm[i], 1 / 1), text_type="median", border=0, color_nan=(255, 255, 255)) for i in range(len(video_mm))]
     for i, img_mueller_vis in enumerate(img_mueller_vis_list):
         filename_png = f"{dir_dst}/images_median/{filename_dst_stem}_{i:03d}.png"
         Path(filename_png).parent.mkdir(exist_ok=True, parents=True)
         cv2.imwrite(filename_png, img_mueller_vis)
 
+    filename_mp4 = f"{dir_dst}/{filename_dst_stem}_median.mp4"
+    vidwrite(filename_mp4, img_mueller_vis_list, framerate=30)
+
     img_mueller_vis_list = [ee.mueller_image(pa.gammaCorrection(video_mm[i], 1 / 1), text_type="none", border=0, color_nan=(255, 255, 255)) for i in range(len(video_mm))]
     for i, img_mueller_vis in enumerate(img_mueller_vis_list):
         filename_png = f"{dir_dst}/images/{filename_dst_stem}_{i:03d}.png"
         Path(filename_png).parent.mkdir(exist_ok=True, parents=True)
         cv2.imwrite(filename_png, img_mueller_vis)
+
+    filename_mp4 = f"{dir_dst}/{filename_dst_stem}.mp4"
+    vidwrite(filename_mp4, img_mueller_vis_list, framerate=30)
