9e5d6449b133a0e1d48cfe8b5f54c19264f4daae
Ryota Maeda <42407614+elerac@users.noreply.github.com>
2024-11-19 16:25:19 +0900

    Update calib_qwp_offset.py

--- scripts/calib_qwp_offset.py
+++ scripts/calib_qwp_offset.py
@@ -10,34 +10,46 @@ import datetime
 def main():
     np.random.seed(0)
     np.set_printoptions(precision=3, suppress=True)
-    filename_raw = "./recordings/calib_qwp_offset/recording_2024-10-15_16-58-31.raw"  # QWP(pi/4)
+    filename_raw = "./recordings/calib_qwp_offset/recording_2024-10-19_17-17-53.raw"  # QWP(pi/4)
+    filename_raw = "./recordings/calib_qwp_offset/recording_2024-10-20_17-41-08.raw"  # QWP, 200x200
+    # filename_raw = "./recordings/recording_2024-11-05_17-17-28.raw" # air
+
+    print(f"Reading {filename_raw}")
 
     events = ee.read_event(filename_raw)
     bias_diff = int(events["bias_diff"])
     bias_diff_on = int(events["bias_diff_on"])
     bias_diff_off = int(events["bias_diff_off"])
+    bias_fo = int(events["bias_fo"])
+    bias_hpf = int(events["bias_hpf"])
+    bias_refr = int(events["bias_refr"])
     print(f"Bias diff: {bias_diff}")
     print(f"Bias diff on: {bias_diff_on}")
     print(f"Bias diff off: {bias_diff_off}")
+    print(f"Bias fo: {bias_fo}")
+    print(f"Bias hpf: {bias_hpf}")
+    print(f"Bias refr: {bias_refr}")
     width = events["width"]
     height = events["height"]
     filename_C_on = f"calib/thresh/on/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
     filename_C_off = f"calib/thresh/off/diff{bias_diff}_diff_on{bias_diff_on}_diff_off{bias_diff_off}.npy"
     img_C_on = np.load(filename_C_on).astype(np.float32)
     img_C_off = np.load(filename_C_off).astype(np.float32)
+    img_C_on = np.clip(img_C_on, 1e-9, None)
+    img_C_off = np.clip(img_C_off, None, -1e-9)
 
     ellipsometry_eventmaps = ee.construct_dataframes(events["x"], events["y"], events["t"], events["p"], width, height, events["trig_t"], events["trig_p"], img_C_on, img_C_off)
 
     x = width // 2
     y = height // 2
-    theta, dlogI, weights, phi_offset = ellipsometry_eventmaps[0].get(x, y)
+    theta, dlogI, weights, phi_offset = ellipsometry_eventmaps[1].get(x, y)
 
     h_error = 360
     w_error = 360
     img_error = np.zeros((h_error, w_error))
     phi1_candidates = np.linspace(0, np.pi, h_error, endpoint=False)
     phi2_candidates = np.linspace(0, np.pi, w_error, endpoint=False)
-    M_ref = pa.depolarizer(0.95) @ pa.qwp(np.pi / 4) @ pa.depolarizer(0.95)
+    M_ref = pa.depolarizer(0.8) @ pa.qwp(np.pi / 4)
     for j, phi1 in enumerate(phi1_candidates):
         for i, phi2 in enumerate(phi2_candidates):
             n_coffs, d_coffs = ee.calculate_ndcoffs(theta, phi1, phi2 - phi_offset * 5)
@@ -51,8 +63,10 @@ def main():
     phi2 = phi2_candidates[index[1]]
     print("Calibration")
     print(f"error (min): {np.min(img_error):.2f}")
-    print(f"phi1: {np.rad2deg(phi1):.2f}")
-    print(f"phi2: {np.rad2deg(phi2):.2f}")
+    print(f"phi1: {np.rad2deg(phi1):.2f} ({phi1:.3f})")
+    print(f"phi2: {np.rad2deg(phi2):.2f} ({phi2:.3f})")
+    print(f"phi1 + pi/2: {np.rad2deg(phi1 + np.pi/2) % 180:.2f} ({(phi1 + np.pi/2) % np.pi:.3f})")
+    print(f"phi2 + pi/2: {np.rad2deg(phi2 + np.pi/2) % 180:.2f} ({(phi2 + np.pi/2) % np.pi:.3f})")
 
     # Write the result to calib/qwp_offset.txt
     datetime_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -83,15 +97,22 @@ def main():
     cv2.imwrite(filename_png, img_error_vis)
 
     # Plot again
+    plt.plot(theta, dlogI, "o", label="data")
+    theta = np.linspace(0, np.pi, 360).astype(np.float32)
     n_coffs, d_coffs = ee.calculate_ndcoffs(theta, phi1, phi2 - phi_offset * 5)
     dlogI_pred = (n_coffs @ M_ref.flatten()) / (d_coffs @ M_ref.flatten())
-    plt.plot(theta, dlogI, "o", label="data")
     plt.plot(theta, dlogI_pred, label="pred")
     plt.xlim(0, np.pi)
     vmax = np.max(np.abs(dlogI))
     plt.ylim(-vmax * 1.05, vmax * 1.05)
     filename_pdf = "calib/qwp_offset/qwp_offset_plot.pdf"
     plt.savefig(filename_pdf, bbox_inches="tight", dpi=500)
+
+    img_colorbar = np.linspace(0, 1, 256).reshape(1, -1)
+    img_colorbar = np.repeat(img_colorbar, 16, axis=0)
+    img_colorbar_jet = pa.applyColorMap(img_colorbar, "jet", vmin=0, vmax=1)
+    filename_colorbar = "calib/qwp_offset/qwp_offset_colorbar.png"
+    cv2.imwrite(filename_colorbar, img_colorbar_jet)
 
 
 if __name__ == "__main__":
