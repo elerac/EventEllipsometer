b07fe1a87c2adea445e140bb9f2302a9b9dc5e1c
Ryota MAEDA <maenosuke079@gmail.com>
2024-11-23 18:15:17 +0900

    Update __init__.py to import metavision as optional

--- src/eventellipsometry/__init__.py
+++ src/eventellipsometry/__init__.py
@@ -1,11 +1,17 @@
-from .event_record import *
-from .event_io import *
 from .eventmap import EventMapPy
 from .utils_mueller import *
 from . import utils
 
 import importlib.util
 import warnings
+
+is_metavision_available = importlib.util.find_spec("metavision_core") is not None
+if is_metavision_available:
+    from .event_record import *
+    from .event_io import *
+else:
+    warnings.warn("Metavision is not available.", stacklevel=2)
+
 
 # Import the C++ extension if it is available
 is_cpp_extension_available = importlib.util.find_spec("eventellipsometry._eventellipsometry_impl") is not None
